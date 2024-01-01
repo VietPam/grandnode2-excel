f8adfdbc2d049adbc4fd2a6d1c3dd7a3c48a9baa
support <support@grandnode.com>
2024-01-01 19:52:52 +0100

    Update plugin

---
+++ src/Plugins/Theme.Minimal/PluginViewLocationExpander.cs
@@ -0,0 +1,27 @@
+﻿using Grand.Web.Common.Themes;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Theme.Minimal;
+
+public class PluginViewLocationExpander: IViewLocationExpander
+{
+    private const string ThemeKey = "Plugin";
+
+    public void PopulateValues(ViewLocationExpanderContext context)
+    {
+        context.Values[ThemeKey] = "Plugin";
+    }
+
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+        if (context.Values.TryGetValue("Theme", out var theme) && theme == "Minimal")
+        {
+            viewLocations = new[] {
+                    $"/Plugins/Theme.Minimal/Views/Minimal/{{1}}/{{0}}.cshtml",
+                }
+                .Concat(viewLocations);
+        }
+
+        return viewLocations;
+    }
+}
--- src/Plugins/Theme.Minimal/StartupApplication.cs
+++ src/Plugins/Theme.Minimal/StartupApplication.cs
@@ -2,6 +2,7 @@
 using Grand.Web.Common.Themes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,9 +13,23 @@ public class StartupApplication : IStartupApplication
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IThemeView, MinimalThemeView>();
+        
+        services.Configure<RazorViewEngineOptions>(options =>
+        {
+            /*
+            options.ViewLocationFormats.Clear();
+            options.ViewLocationFormats.Add(@"C:\Projects\Grandnode\grandnode2\src\Web\Grand.Web\Plugins\Theme.Minimal\{1}\{0}.cshtml");
+            options.ViewLocationFormats.Add(@"C:\Projects\Grandnode\grandnode2\src\Web\Grand.Web\Plugins\Theme.Minimal\");
+            options.ViewLocationFormats.Add(@"/Plugins/Theme.Minimal/");
+            options.ViewLocationFormats.Add(@"/Plugins/Theme.Minimal/Views/{1}/{0}.cshtml");
+            options.ViewLocationFormats.Add(@"/Plugins/Theme.Minimal/Views/Minimal/{1}/{0}.cshtml");
+        */
+            options.ViewLocationExpanders.Add(new PluginViewLocationExpander());
+
+        });
     }
 
-    public int Priority => 10;
+    public int Priority => 2000;
 
     public void Configure(IApplicationBuilder application, IWebHostEnvironment webHostEnvironment)
     {
